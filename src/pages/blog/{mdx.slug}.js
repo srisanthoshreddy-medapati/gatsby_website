f3500a1f9a18fcc1d2c3d5862e2228741c79ed08
srisanthoshreddy-medapati <mssreddy1932@gmail.com>
2021-08-18 13:05:57 +0530

    gatsby site

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -4,21 +4,29 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
-onst BlogPost = ({ data }) => {
-  const image = getImage(data.mdx.frontmatter.hero_image) 
+
+const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <p>{data.mdx.frontmatter.date}</p>
       <GatsbyImage
         image={image}
         alt={data.mdx.frontmatter.hero_image_alt}
       />
+      <p>
+        Photo Credit:{" "}
+        <Link to={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </Link>
+      </p>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
     </Layout>
   )
-}
+  }
 export const query = graphql`
   query($slug: String) {
     mdx(slug: {eq: $slug}) {
