1eb763e99f70545efb06334ea1972ca301bbf0db
srisanthoshreddy-medapati <mssreddy1932@gmail.com>
2021-08-18 11:16:03 +0530

    gatsby site

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,18 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image) 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}
+    />
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -14,14 +20,21 @@ const BlogPost = ({ data }) => {
   )
 }
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
-      }
+  query($slug: String) {
+    mdx(slug: {eq: $slug}) {
       body
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
     }
   }
-  `
 export default BlogPost
